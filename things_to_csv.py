b44086f3e69da57ed790f4ad8eea5401b465a8f8
nathanabram <nathan@nathanabram.com>
2022-04-11 17:36:26 -0400

    impliment tasks export

--- things_to_csv.py
+++ things_to_csv.py
@@ -1,27 +1,65 @@
 import things
 import csv
-#want one page of tasks, one page of projects, one page of areas, one page of tasks
-#want a toggle for including completed, removed, and canceled tasks
-allTasks = things.tasks(status=None)
+import sys
+from datetime import datetime
 
-keys = ['uuid', 'type', 'title', 'status', 'start', 'area', 'area_title', 'project', 'project_title', 'heading', 'heading_title', 'notes', 'tags', 'start', 'start_date', 'deadline', 'checklist', 'stop_date', 'created', 'modified', 'index', 'today_index']
-with open('todos.csv', 'w', newline='') as output_file:
-    dict_writer = csv.DictWriter(output_file, keys)
-    dict_writer.writeheader()
-    dict_writer.writerows(allTasks)
+includeCompleted = sys.argv[1] if len(sys.argv) > 1 else False
+includeCanceled = sys.argv[2] if len(sys.argv) > 2 else False
 
 
-def getTasks(includeCompleted = False, includeCanceled = False, **kwargs):
+def getTasks(includeCompleted, includeCanceled):
     completed = []
     canceled = []
 
     if includeCompleted:
-        completed = things.tasks(status = 'completed', include_items=True)
+        completed = things.tasks(status="completed", include_items=True)
     if includeCanceled:
-        canceled = things.tasks(status = 'canceled', include_items=True)
+        canceled = things.tasks(status="canceled", include_items=True)
 
     tasks = things.tasks(include_items=True)
-
     tasks = tasks + completed + canceled
 
     return tasks
+
+
+usersTasks = getTasks(includeCompleted, includeCanceled)
+
+# format string for output
+completed = "_completed" if includeCompleted else ""
+canceled = "_canceled" if includeCanceled else ""
+now = datetime.now()
+
+
+filename = f"things_tasks_{now.year}-{now.month}-{now.day}_{now.hour}-{now.minute}_incomplete{completed}{canceled}.csv"
+
+
+keys = [
+    "uuid",
+    "type",
+    "title",
+    "status",
+    "start",
+    "area",
+    "area_title",
+    "project",
+    "project_title",
+    "heading",
+    "heading_title",
+    "notes",
+    "items",
+    "tags",
+    "start",
+    "start_date",
+    "deadline",
+    "checklist",
+    "stop_date",
+    "created",
+    "modified",
+    "index",
+    "today_index",
+]
+
+with open(filename, "w", encoding="utf-8-sig", newline="") as output_file:
+    dict_writer = csv.DictWriter(output_file, keys)
+    dict_writer.writeheader()
+    dict_writer.writerows(usersTasks)
