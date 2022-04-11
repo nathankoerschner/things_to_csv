0591b458a7e0ec67ec8c51368ae48a465f2ebb40
nathanabram <nathan@nathanabram.com>
2022-04-11 18:28:32 -0400

    init areas exporter

---
+++ areas_to_csv.py
@@ -0,0 +1,42 @@
+import things
+import csv
+from datetime import datetime
+
+usersTags = things.tags()
+
+# format string for output
+now = datetime.now()
+filename = f"things_tags_{now.year}-{now.month}-{now.day}_{now.hour}-{now.minute}.csv"
+
+
+keys = [
+    "uuid",
+    "type",
+    "title",
+    "shortcut",
+    "status",
+    "start",
+    "area",
+    "area_title",
+    "project",
+    "project_title",
+    "heading",
+    "heading_title",
+    "notes",
+    "items",
+    "tags",
+    "start",
+    "start_date",
+    "deadline",
+    "checklist",
+    "stop_date",
+    "created",
+    "modified",
+    "index",
+    "today_index",
+]
+
+with open(filename, "w", encoding="utf-8-sig", newline="") as output_file:
+    dict_writer = csv.DictWriter(output_file, keys)
+    dict_writer.writeheader()
+    dict_writer.writerows(usersTags)
